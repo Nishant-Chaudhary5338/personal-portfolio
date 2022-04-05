24615035be87049b27a199dade385ec1b784a979
Nishant-Chaudhary5338 <nishantchaudhary5338@gmail.com>
2022-04-05 22:47:59 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="h-screen bg-gray-300 flex justify-center items-center">
+      <h1 className="text-9xl">CodeYogi</h1>
+    </div>
+  );
+}
+
+export default App;
